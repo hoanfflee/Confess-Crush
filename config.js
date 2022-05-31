944d002237fcf7af5095942a7e6ce6de8bb3649c
hoanfflee <106627537+hoanfflee@users.noreply.github.com>
2022-05-31 20:31:14 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
     titleWeb: "Demo",
     introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introDesc: `Câu hỏi dành cho Xuân Thu,
+    btnIntro: 'Hahah',
+    title: 'Dành 1 đêm để tạo code này',
+    desc: 'Chúc bạn buổi sáng vui vẻ ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
